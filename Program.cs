a248b33401d2c9963e3b5e059117c96d0eea844b
Jeff Stinson <jeffstinson@hotmail.com>
2023-05-17 22:59:51 -0700

    add basic console read and write

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+
+            string name = Console.ReadLine();
+
+            Console.Write($"hello {name}");
+        }
+    }
+}
